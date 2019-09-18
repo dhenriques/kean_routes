3e24963a768c8d45c79848a2051d3e8b1624605b
dhenriques <36280181+dhenriques@users.noreply.github.com>
2019-09-18 13:40:36 -0400

    Added space between title and form

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ class App extends Component {
     return (
       <div class="level" className="App">
         <div class="container">
-          <h1 class="title">Hello, React!</h1>
+          <div class="content">
+            <h1 class="title">Hello, React!</h1>
+          </div>
         </div>
       </div>
     );
